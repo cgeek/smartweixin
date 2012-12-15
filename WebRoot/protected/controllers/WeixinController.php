c13c2136b0a89e24c29961621f26866294f6fa67
cgeek <cgeek.share@gmail.com>
2012-12-15 14:58:31 +0800

    Weixin valid

--- WebRoot/protected/controllers/WeixinController.php
+++ WebRoot/protected/controllers/WeixinController.php
@@ -10,6 +10,42 @@ class WeixinController extends Controller
 	}
 
 	public function actionMessage()
+	{
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr)){
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";             
+				if(!empty( $keyword ))
+                {
+              		$msgType = "text";
+                	$contentStr = "Welcome to wechat world!";
+                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                	echo $resultStr;
+                }else{
+                	echo "Input something...";
+                }
+
+        } else {
+        	echo "";
+        	exit;
+        }
+	}
+
+	public function actionValid()
     {
         $echoStr = $_GET["echostr"];
 
@@ -19,6 +55,7 @@ class WeixinController extends Controller
         	exit;
         }
 	}
+
 
 	private function checkSignature()
 	{
